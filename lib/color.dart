197d6809ebe2498d5fef8781a2e1ed7c475d9b61
Cantidio Fontes <aniquilatorbloody@gmail.com>
2013-05-13 19:36:13 -0300

    Color Class updated.
    Operators added: ==, + and - for color objects.
    Tests updated.

--- lib/color.dart
+++ lib/color.dart
@@ -6,27 +6,55 @@ part of gorgon;
 /**
  * Class that represents a color
  */
-class Color 
+class Color
 {
-  int r;
-  int g;
-  int b;
-  int a;
+  int _r;
+  int _g;
+  int _b;
+  int _a;
+
+  int get r => _r;
+  int get g => _g;
+  int get b => _b;
+  int get a => _a;
+
+  set r(int r) => _r = (r >= 0) ? (r <= 255 ) ? r : 255 : 0;
+  set g(int g) => _g = (g >= 0) ? (g <= 255 ) ? g : 255 : 0;
+  set b(int b) => _b = (b >= 0) ? (b <= 255 ) ? b : 255 : 0;
+  set a(int a) => _a = (a >= 0) ? (a <= 255 ) ? a : 255 : 0;
 
   /**
-   * Method that describes the Mirroring Object returning a [String].
+   * Method that describes the Color Object returning a [String].
    */
   String toString() => "Color(r:$r, g:$g, b:$b, a:$a)";
+
   /**
    * Constructor
+   *
+   * You should pass the components you whish to set.
+   * For setting the red component you should set the[r] param as an int
+   * For setting the green component you should set the[g] param as an int
+   * For setting the blue component you should set the[b] param as an int
+   * For setting the alpha component you should set the[a] param as an int
    */
-  Color([ int r=0, int g=0, int b=0, int a= 255 ])
+  Color({ int r:0, int g:0, int b:0, int a: 255 })
   {
-    this.r = (r >= 0) ? r : 0;
-    this.g = (g >= 0) ? g : 0;
-    this.b = (b >= 0) ? b : 0;
-    this.a = (a >= 0) ? a : 0;
-  }  
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+  }
+  /**
+   * Operator that sums 2 [Color]s.
+   */
+  operator +(Color other) => new Color( r: r + other.r, g: g + other.g, b: b + other.b, a: a + other.a );
+  /**
+   * Operator that substracts one [Color] from the [other] [Color].
+   */
+  operator -(Color other) => new Color( r: r - other.r, g: g - other.g, b: b - other.b, a: a - other.a );
+  /**
+   * Operator that checks if 2 [Color]s are have the same values.
+   */
+  operator ==(Color other) => r == other.r && g == other.g && b == other.b && a == other.a;
 }
-
 
--- test/color_test.dart
+++ test/color_test.dart
@@ -9,34 +9,183 @@ import "package:gorgon/gorgon.dart";
 
 void main()
 {
-  test
-  (
-      "Color members value check when using the empty Constructor ", (){
-        Color color = new Color();
+  test( "Color members value check when using the empty Constructor ", (){
+    Color color = new Color();
 
-        expect( color.r, equals( 0 ) );
-        expect( color.g, equals( 0 ) );
-        expect( color.b, equals( 0 ) );
-        expect( color.a, equals( 255 ) );
-  });
-  test
-  (
-      "Color members value check when setting the values in the Constructor ", (){
-        Color color = new Color( 255, 200, 0, 175 );
+    expect( color.r, equals( 0 ) );
+    expect( color.g, equals( 0 ) );
+    expect( color.b, equals( 0 ) );
+    expect( color.a, equals( 255 ) );
 
-        expect( color.r, equals( 255 ) );
-        expect( color.g, equals( 200 ) );
-        expect( color.b, equals( 0 ) );
-        expect( color.a, equals( 175 ) );
   });
-  test
-  (
-      "Color members value check when setting negative values in the Constructor ", (){
-        Color color = new Color( -255, 200, 10, -175 );
 
-        expect( color.r, equals( 0 ) );
-        expect( color.g, equals( 200 ) );
-        expect( color.b, equals( 10 ) );
-        expect( color.a, equals( 0 ) );
+  test( "Color members value check when setting the values in the Constructor ", (){
+    Color color = new Color( r: 255, g: 200, b: 0, a: 175 );
+
+    expect( color.r, equals( 255 ) );
+    expect( color.g, equals( 200 ) );
+    expect( color.b, equals( 0 ) );
+    expect( color.a, equals( 175 ) );
   });
+
+  test( "Color members value check when setting negative values in the Constructor ", (){
+    Color color = new Color( r:-255, g:200, b:10, a:-175 );
+
+    expect( color.r, equals( 0 ) );
+    expect( color.g, equals( 200 ) );
+    expect( color.b, equals( 10 ) );
+    expect( color.a, equals( 0 ) );
+  });
+  test( "Color members value check when setting values over 255 in the Constructor ", (){
+    Color color = new Color( r: 255, g:400, b:100, a: 750 );
+
+    expect( color.r, equals( 255 ) );
+    expect( color.g, equals( 255 ) );
+    expect( color.b, equals( 100 ) );
+    expect( color.a, equals( 255 ) );
+  });
+
+  test( "Color getter r", (){
+    Color red = new Color( r: 255 );
+
+    expect( red.r, equals( 255 ) );
+  });
+
+  test( "Color getter g", (){
+    Color green = new Color( g: 255 );
+
+    expect( green.g, equals( 255 ) );
+  });
+
+  test( "Color getter b", (){
+    Color blue = new Color( b: 255 );
+
+    expect( blue.b, equals( 255 ) );
+  });
+
+  test( "Color getter a", (){
+    Color alpha = new Color( a: 255 );
+
+    expect( alpha.a, equals( 255 ) );
+  });
+
+  test( "Color setter r normal value", (){
+    Color red = new Color();
+    red.r = 255;
+
+    expect( red.r, equals( 255 ) );
+  });
+
+  test( "Color setter r negative value", (){
+    Color red = new Color();
+    red.r = -255;
+
+    expect( red.r, equals( 0 ) );
+  });
+
+  test( "Color setter r value over 255", (){
+    Color red = new Color();
+    red.r = 355;
+
+    expect( red.r, equals( 255 ) );
+  });
+
+  test( "Color setter g normal value", (){
+    Color green = new Color();
+    green.g = 255;
+
+    expect( green.g, equals( 255 ) );
+  });
+
+  test( "Color setter g negative value", (){
+    Color green = new Color();
+    green.g = -255;
+
+    expect( green.g, equals( 0 ) );
+  });
+
+  test( "Color setter g value over 255", (){
+    Color green = new Color();
+    green.g = 355;
+
+    expect( green.g, equals( 255 ) );
+  });
+
+  test( "Color setter b normal value", (){
+    Color blue = new Color();
+    blue.b = 255;
+
+    expect( blue.b, equals( 255 ) );
+  });
+
+  test( "Color setter b negative value", (){
+    Color blue = new Color();
+    blue.b = -255;
+
+    expect( blue.b, equals( 0 ) );
+  });
+
+  test( "Color setter b value over 255", (){
+    Color blue = new Color();
+    blue.b = 355;
+
+    expect( blue.b, equals( 255 ) );
+  });
+
+  test( "Color setter a normal value", (){
+    Color alpha = new Color();
+    alpha.a = 255;
+
+    expect( alpha.a, equals( 255 ) );
+  });
+
+  test( "Color setter a negative value", (){
+    Color alpha = new Color();
+    alpha.a = -255;
+
+    expect( alpha.a, equals( 0 ) );
+  });
+
+  test( "Color setter a value over 255", (){
+    Color alpha = new Color();
+    alpha.a = 355;
+
+    expect( alpha.a, equals( 255 ) );
+  });
+
+  test( "Color Operator ==", (){
+    Color colorA = new Color( r:50, g:100, b: 150, a: 200 );
+    Color colorB = new Color( r:50, g:100, b: 150, a: 200 );
+
+    expect( colorA, equals( colorB ) );
+  });
+
+  test( "Color Operator +", (){
+    Color colorA = new Color( r:100, g:100 );
+    Color colorB = new Color( r:100, b:150 );
+
+    expect( colorA + colorB, equals( new Color( r: 200, g: 100, b: 150 ) ) );
+  });
+
+  test( "Color Operator + with values that surpass 255", (){
+    Color colorA = new Color( r:150, g:150, b: 150, a: 255 );
+    Color colorB = new Color( r:150, g:150, b: 150, a: 255 );
+
+    expect( colorA + colorB, equals( new Color( r: 255, g: 255, b: 255, a: 255 ) ) );
+  });
+
+  test( "Color Operator -", (){
+    Color colorA = new Color( r:100, g:100 );
+    Color colorB = new Color( r:100, g:10 );
+
+    expect( colorA - colorB, equals( new Color( r: 0, g: 90, b: 0, a: 0 ) ) );
+  });
+
+  test( "Color Operator - with values when subbed become less than 0", (){
+    Color colorA = new Color( r:150, g:150, b: 150, a: 150 );
+    Color colorB = new Color( r:250, g:250, b: 250, a: 255 );
+
+    expect( colorA - colorB, equals( new Color( r: 0, g: 0, b: 0, a: 0 ) ) );
+  });
+
 }
