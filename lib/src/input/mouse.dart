85430dad054a975161a8c5c13a85efa369e62db8
Cantidio Fontes <aniquilatorbloody@gmail.com>
2013-08-29 16:02:26 -0300

    First mouse draft.

---
+++ lib/src/input/mouse.dart
@@ -0,0 +1,112 @@
+/**
+ * Copyright (C) 2013 Cantidio Fontes
+ * For conditions of distribution and use, see copyright notice in LICENSE.txt
+ */
+part of gorgon;
+
+/**
+ * Class that represents a keyboard.
+ */
+class Mouse
+{
+  Map<int,bool> _buttonBuffer;
+  List _keyDownCallback;
+  List _keyUpCallback;
+  List _onMoveCallback;
+  Point2D _mousePosition;
+
+
+  /// Returns all keycodes being pressed.
+  List<int> get pressedButtons => _keyBuffer.keys.toList(growable: false);
+
+  /**
+   * [Mouse] constructor.
+   */
+  Mouse()
+  {
+    _mousePosition   = new Point2D.zero();
+    _buttonBuffer    = new Map<int,bool>();
+    _keyDownCallback = new List();
+    _keyUpCallback   = new List();
+    _onMoveCallback  = new List();
+    _registerEvents();
+  }
+
+  /**
+   * Method that clears the key buffer.
+   */
+  void clearKeyBuffer()
+  {
+    _keyBuffer.clear();
+  }
+
+  /**
+   * Method that register a [callback] that will be called when a key is Up.
+   */
+  void onKeyUp( callback )
+  {
+    _keyUpCallback.add( callback );
+  }
+
+  /**
+   * Method that register a [callback] that will be called when a key is Down.
+   */
+  void onKeyDown( callback )
+  {
+    _keyDownCallback.add( callback );
+  }
+
+  /**
+   * Operator that returns [true] if the [key] isß pressed [false] otherwise.
+   */
+  bool operator []( int key )
+  {
+    return _keyBuffer[key] != null ? _keyBuffer[key] : false;
+  }
+
+  /**
+   * Method that registers the necessary events
+   */
+  void _registerEvents()
+  {
+    window.onMouseMove.listen ((event) => _onMove      ( event ) );
+    window.onMouseWheel.listen((event) => _onWheel     ( event ) );
+    window.onMouseDown.listen ((event) => _onButtonDown( event ) );
+    window.onMouseUp.listen   ((event) => _onButtonUp  ( event ) );
+  }
+
+  void _onMove( MouseEvent event )
+  {
+    Point2D newPos = new Point2D( event.clientX, event.clientY );
+    if( newPos != _mousePosition )
+    {
+      _mousePosition = newPos;
+    }
+  }
+
+  /**
+   * Method called everytime a Button is pressed down.
+   */
+  void _onButtonDown( MouseEvent event )
+  {
+    if( _buttonBuffer[event.button] != true )
+    {
+      _buttonBuffer[event.button] = true;
+      _buttonDownCallback.forEach((e) => e(event.button));
+    }
+  }
+  /**
+   * Method called everytime a Button is released
+   */
+  void _onKeyUp( MouseEvent event )
+  {
+    if( _buttonBuffer[event.button] != null )
+    {
+      _buttonBuffer.remove( event.button );
+      _buttonUpCallback.forEach((e) => e(event.button));
+    }
+  }
+
+}
+
+
