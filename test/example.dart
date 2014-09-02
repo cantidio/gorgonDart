9ab0eaf8c0dfc9978c74bcfd1f5288569e49775a
Cantidio Fontes <aniquilatorbloody@gmail.com>
2014-09-02 15:18:55 -0300

    Update example.dart

--- test/example.dart
+++ test/example.dart
@@ -19,14 +19,17 @@ main()
   sp.onLoad.then((_){
     num ang = 0.0;
     num scale = 1.0;
+    int scale_mod = 1;
+
       anim.changeAnimation("walk");
       Timer timer = new Timer.periodic( const Duration(milliseconds: 1000~/60), (_) {
          display.clear();
          anim.runStep();
-         anim.draw(new Point2D(80,80), rotation: ang, scale: scale);
+         anim.draw(new Point2D(180,180), rotation: ang, scale: scale);
          ang += 0.8;
-         scale += 0.1;
-         if(scale >= 3.0) scale = 0.3;
+         scale += 0.05 * scale_mod;
+         if(scale >= 3.0) scale_mod = -1;
+         if(scale <= 0.5) scale_mod = 1;
       });
   });
 }
