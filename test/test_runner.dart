e0ae49375eb4dda078abac39910f780e08c17cbe
Cantidio Fontes <aniquilatorbloody@gmail.com>
2014-09-02 15:08:02 -0300

    Update test_runner.dart
    Removed the commented example code as this just runs the tests right now. #12

--- test/test_runner.dart
+++ test/test_runner.dart
@@ -8,29 +8,10 @@ import 'graphic/test.dart'       as graphic;
 import 'audio/test.dart'         as audio;
 import 'input/test.dart'       as input;
 
-import 'package:gorgon/gorgon.dart';
-import 'dart:html';
 main()
 {
   useHtmlConfiguration();
   graphic.main();
   audio.main();
   input.main();
-
-//  Display display = new Display( query("#display"), width: 320, height: 240 );
-//  Spritepack sp = new Spritepack.fromSpriteSheet("resources/chico/chico.png", {
-//    "walk":[
-//      { "xstart": 0, "ystart":0, "width": 79, "height": 79, "xoffset" : -32, "yoffset" : -64 },
-//      { "xstart": 79, "ystart":20, "width": 79, "height": 39, "xoffset" : -32, "yoffset" : -44 },]
-//  });
-//  sp.onLoad.then((_){
-//    int line=1;
-//    sp.forEachSprite((s){
-//      s.flipHV().then((sr){
-//        sr.draw(new Point2D(80,line* 80));
-//        line++;
-//      });
-//    });
-//  });
-
 }
